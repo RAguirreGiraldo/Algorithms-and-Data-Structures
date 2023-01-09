a5f390f7ae48ec3dafb4ce509ce85b20cfd506d9
RAguirreGiraldo <raguirregiraldo@gmail.com>
2023-01-09 14:30:12 -0600

    Add project files.

---
+++ Lab1/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+Console.WriteLine("Enter a Number of the Size of the World");
+int sizeOFarray = Int32.Parse(Console.ReadLine());
+
+string[] arrayNewWord = new string[sizeOFarray];
+
+for (int i = 0; i < arrayNewWord.Length; i++);
+{
+    Console.WriteLine("Enter a new letter ");
+    string arrayCreateNewWord = Console.ReadLine();
+}
+
+Console.WriteLine(sizeOFarray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+String nombre1, nombre2;
+int edad1, edad2;
+String linea;
+Console.Write("Ingrese el nombre:");
+nombre1 = Console.ReadLine();
+Console.Write("Ingrese edad:");
+linea = Console.ReadLine();
+edad1 = int.Parse(linea);
+Console.Write("Ingrese el nombre:");
+nombre2 = Console.ReadLine();
+Console.Write("Ingrese edad:");
+linea = Console.ReadLine();
+edad2 = int.Parse(linea);
+Console.Write("La persona de mayor edad es:");
+if (edad1 > edad2)
+{
+    Console.Write(nombre1);
+}
+else
+{
+    Console.Write(nombre2);
+}
+Console.ReadKey();
+        }
+*/
