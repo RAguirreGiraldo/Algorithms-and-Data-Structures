f3d52dcada6b6f7dd4d861bf5aeb5a96a4124416
RAguirreGiraldo <raguirregiraldo@gmail.com>
2023-01-12 20:40:54 -0600

    Create a variable to storage a number of length of my array

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,16 +1,31 @@
 ﻿
-Console.WriteLine("Enter a Number of the Size of the World");
-int sizeOFarray = Int32.Parse(Console.ReadLine());
+/* Count Words */
 
-string[] arrayNewWord = new string[sizeOFarray];
+string[] arrayNewWord;  //Creation of my array
 
-for (int i = 0; i < arrayNewWord.Length; i++);
+Console.WriteLine("Number of words you wish to enter "); 
+int sizeOfarraywords = Int32.Parse(Console.ReadLine()); // Create variable type Int to number of the lengh of my array.
+
+arrayNewWord = new string[sizeOfarraywords];  //initializate a array with size of declarate by user.
+
+//Input the worlds 
+for (int i = 0; i < arrayNewWord.Length; i++)
 {
-    Console.WriteLine("Enter a new letter ");
-    string arrayCreateNewWord = Console.ReadLine();
+    Console.WriteLine("Enter a Word {0}: ", i + 1); //World
+    arrayNewWord[i] = Console.ReadLine();
 }
 
-Console.WriteLine(sizeOFarray);
+// Print word
+Console.WriteLine();
+Console.WriteLine(" Press any key to show the words ");
+Console.ReadKey();
+
+for (int i = 0; i < sizeOfarraywords; i++)
+{
+    Console.WriteLine("{0}. {1}", i + 1, arrayNewWord[i]);
+}
+ 
+
 
 
 
