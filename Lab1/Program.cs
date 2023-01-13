fc6639140fe719b6d894af59f6dc6ba06220ac86
RAguirreGiraldo <raguirregiraldo@gmail.com>
2023-01-12 20:49:14 -0600

    initializate a array with size of declarate by user, Input the worlds

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,7 +4,7 @@
 string[] arrayNewWord;  //Creation of my array
 
 Console.WriteLine("Number of words you wish to enter "); 
-int sizeOfarraywords = Int32.Parse(Console.ReadLine()); // Create variable type Int to number of the lengh of my array.
+int sizeOfarraywords = Int32.Parse(Console.ReadLine()); // Create variable type Int to number of the length of my array.
 
 arrayNewWord = new string[sizeOfarraywords];  //initializate a array with size of declarate by user.
 
@@ -14,60 +14,3 @@ for (int i = 0; i < arrayNewWord.Length; i++)
     Console.WriteLine("Enter a Word {0}: ", i + 1); //World
     arrayNewWord[i] = Console.ReadLine();
 }
-
-// Print word
-Console.WriteLine();
-Console.WriteLine(" Press any key to show the words ");
-Console.ReadKey();
-
-for (int i = 0; i < sizeOfarraywords; i++)
-{
-    Console.WriteLine("{0}. {1}", i + 1, arrayNewWord[i]);
-}
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-String nombre1, nombre2;
-int edad1, edad2;
-String linea;
-Console.Write("Ingrese el nombre:");
-nombre1 = Console.ReadLine();
-Console.Write("Ingrese edad:");
-linea = Console.ReadLine();
-edad1 = int.Parse(linea);
-Console.Write("Ingrese el nombre:");
-nombre2 = Console.ReadLine();
-Console.Write("Ingrese edad:");
-linea = Console.ReadLine();
-edad2 = int.Parse(linea);
-Console.Write("La persona de mayor edad es:");
-if (edad1 > edad2)
-{
-    Console.Write(nombre1);
-}
-else
-{
-    Console.Write(nombre2);
-}
-Console.ReadKey();
-        }
-*/
